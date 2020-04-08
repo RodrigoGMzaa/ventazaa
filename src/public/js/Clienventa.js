4f0c399d652283868d370208b94965cf38c786b4
Rodrigo Gonzalez <rodrigogonzalezmendozaa@gmail.com>
2020-04-08 14:49:16 -0500

    ventas8

--- src/public/js/Clienventa.js
+++ src/public/js/Clienventa.js
@@ -1,58 +1,51 @@
 window.addEventListener('DOMContentLoaded', e =>{
   fetch('https://cremeria.herokuapp.com/clien/mostrar')
-    .then(res => res.json())
-    .then(data =>{
-        if(data.response == 'success'){
-            const moleszaachila = data.data;
-            
+  .then(res => res.json())
+  .then(data =>{
+      if(data.response == 'success'){
+          const moleszaachila = data.data;
+          
 
 const formulario = document.querySelector('#busquedacliente');
 const boton = document.querySelector('#botonCliente');
 const resultado = document.querySelector('#inputCliente');
 
-
-
 const filtrar = () =>{
-    //console.log(formulario.value);
+  //console.log(formulario.value);
 
-    resultado.innerHTML = "";
+  resultado.innerHTML = "";
 
-    const texto = formulario.value.toLowerCase();
-    
-    for(let moleszaachil of moleszaachila){
+  const texto = formulario.value.toLowerCase();
+
+  for(let moleszaachil of moleszaachila){
       let cliente = newFunction(moleszaachil);
-        if(cliente.indexOf(texto) !== -1){
+      if(cliente.indexOf(texto) !== -1){
             
             document.getElementById("inputCliente").value=moleszaachil.nombre;
             document.getElementById("inputCliente2").value=moleszaachil.telefono;
-           
-            $( "#boto" ).click(function() {
-
-               
-              });
-
               
-        
+          }
+        }
+        if(resultado.innerHTML == ''){
+            resultado.innerHTML +=`
+            <p> Categoria no encontrado</p>
+            `
         }
     }
-    if(resultado.innerHTML == ''){
-        
-    }
-
-}
-
-
-boton.addEventListener('click', filtrar);
-//para filtrar sin precionar buscar 
-formulario.addEventListener('keyup',filtrar);
-//ejecuta la funcion al inicio para que me aparescan todos los datos
-//filtrar();
-
-       }
-    })
-    .catch(err =>{
-        console.error(err);
+    
+    boton.addEventListener('click', filtrar);
+    //para filtrar sin precionar buscar 
+    formulario.addEventListener('keyup',filtrar);
+    //ejecuta la funcion al inicio para que me aparescan todos los datos
+    filtrar();
+    
+            }
+        })
+        .catch(err =>{
+            console.error(err);
+        });
     });
-
-   
-});
+    
+    function newFunction(moleszaachil) {
+        return moleszaachil.nombre.toLowerCase();
+    }
