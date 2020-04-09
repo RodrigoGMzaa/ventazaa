902b6c114ca397da01a05278664e5d2a935bafb9
Rodrigo Gonzalez <rodrigogonzalezmendozaa@gmail.com>
2020-04-09 11:37:20 -0500

    pruebas producto no se repita 1.12

--- src/public/js/productoventa.js
+++ src/public/js/productoventa.js
@@ -85,7 +85,7 @@ $('#tiket').click(function () {
   function agregar(){
     tiketDatos.push(nuevoTiket);
     subtotal = nuevoTiket.precio * nuevoTiket.cantidad
-    document.getElementById("venta_detalle").innerHTML +='<td>'+nuevoTiket.nombre+'</td><td>'+nuevoTiket.precio+'</td><td>'+nuevoTiket.cantidad+'</td>'+'</td><td>'+subtotal+'</td><td><btn value="Eliminar" title="Eliminar" class="btn btn-danger btn-delete"><i class="fas fa-trash"></i></td>'
+    document.getElementById("venta_detalle").innerHTML +='<td>'+nuevoTiket.nombre+'</td><td>'+nuevoTiket.precio+'</td><td>'+nuevoTiket.cantidad+'</td>'+'</td><td>'+subtotal+'</td><td><btn #boton01 value="Eliminar" title="Eliminar" class="btn btn-danger btn-delete"><i class="fas fa-trash"></i></td>'
    
   }
   
@@ -104,6 +104,17 @@ formulario.addEventListener('keyup',filtrar);
     });
 
     //evento eliminar
+    $(document).ready(function() {
+      $("#boton01").click(function(event) {
+        console.log("eliminar");
+      });
+      $("#boton02").click(function(event) {
+        $("#boton02").empty();
+      });
+      $("#boton03").click(function(event) {
+        $(".boton03").empty();
+      });
+
 $(document).on('click', 'btn-delete', function(e){
   e.preventDefault();
   console.log("eliminar");
