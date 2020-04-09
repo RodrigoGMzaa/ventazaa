019486f8ef973a79e11856662b46d4dec79236f9
Rodrigo Gonzalez <rodrigogonzalezmendozaa@gmail.com>
2020-04-09 12:56:27 -0500

    make it yaya

--- src/public/js/productoventa.js
+++ src/public/js/productoventa.js
@@ -85,7 +85,7 @@ $('#tiket').click(function () {
   function agregar(){
     tiketDatos.push(nuevoTiket);
     subtotal = nuevoTiket.precio * nuevoTiket.cantidad
-    document.getElementById("venta_detalle").innerHTML +='<td>'+nuevoTiket.nombre+'</td><td>'+nuevoTiket.precio+'</td><td>'+nuevoTiket.cantidad+'</td>'+'</td><td>'+subtotal+'</td><td><btn #boton01 value="Eliminar" title="Eliminar" class="btn btn-danger btn-delete"><i class="fas fa-trash"></i></td>'
+    document.getElementById("venta_detalle").innerHTML +='<td>'+nuevoTiket.nombre+'</td><td>'+nuevoTiket.precio+'</td><td>'+nuevoTiket.cantidad+'</td>'+'</td><td>'+subtotal+'</td><td><btn id="boton01" value="Eliminar" title="Eliminar" class="btn btn-danger btn-delete"><i class="fas fa-trash"></i></td>'
    
   }
   
@@ -104,10 +104,9 @@ formulario.addEventListener('keyup',filtrar);
     });
 
     //evento eliminar
-   
+  
 
-$(document).on('click', 'btn-delete', function(e){
-  e.preventDefault();
+$('#boton01').click( function(){
   console.log("eliminar");
 });
    
