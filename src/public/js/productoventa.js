a106dfc9d38dc842d83e9250c0714aaff963dac8
Rodrigo Gonzalez <rodrigogonzalezmendozaa@gmail.com>
2020-04-12 17:54:58 -0500

    suma

--- src/public/js/productoventa.js
+++ src/public/js/productoventa.js
@@ -106,11 +106,16 @@ $('#tiket').click(function () {
     subtotal = nuevoTiket.precio * nuevoTiket.cantidad
 
     
-    document.getElementById("venta_detalle").innerHTML +='<td>'+nuevoTiket.nombre+'</td><td>'+nuevoTiket.precio+'</td><td>'+nuevoTiket.cantidad+'</td>'+'</td><td>'+subtotal+'</td><td><btn id="boton01" value="Eliminar" title="Eliminar" class="btn btn-danger btn-delete"><i class="fas fa-trash"></i></td>'
+    document.getElementById("venta_detalle").innerHTML +='<td>'+nuevoTiket.nombre+'</td><td>'+nuevoTiket.precio+'</td><td>'+nuevoTiket.cantidad+'</td>'+'</td><td class="dato">'+subtotal+'</td><td><btn id="boton01" value="Eliminar" title="Eliminar" class="btn btn-danger btn-delete"><i class="fas fa-trash"></i></td>'
     }
     //suma de datos
-    
+    var suma = 0;
+    $('#tabla tb.dato').each(function(){ //filas con clase 'dato', especifica una clase, asi no tomas el nombre de las columnas
+    suma += parseInt($(this).find('td').eq(2).text()||0,10) //numero de la celda 3
+     })
+    console.log(suma)
 
+    //eliminar datos del array
     $('.btn-delete').click( function(e){
       e.preventDefault();
       var row = $(this).parent().parent()[0];
