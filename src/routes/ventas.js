9766f918072a52c23402ed2356e07ec7acb8cd4f
Rodrigo Gonzalez <rodrigogonzalezmendozaa@gmail.com>
2020-04-14 14:56:54 -0500

    a ver que pedo

--- src/routes/ventas.js
+++ src/routes/ventas.js
@@ -1,31 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const VentaController = require("./../controllers/ventas.controller");
-
+const Venta = require("../models/ventaModels");
 
 
 router.get('/ventas', function(req, res) {
     res.render('venta');
   });
+
 router.post("/venta", async (req, res) => {
-  try{
-
-    const {valor_total, cliente, producto, cantidad} = req.body;
-
-    const Ventas = await VentaController.create({...req.body, description});
-
-    producto.map(async producto =>{
-      const productos = new producto({...producto, productos: Productos._id});
-      
-      productos.save().then(producto =>  productos.producto.push(producto));
-    });
-
-    await producto.save();
-
-    return res.send({Ventas});
-  }catch (err){
-    return res.status(400).send({error: 'Error al crera la venta'});
-  }
+  const venta =new Venta(req.body);
+    await venta.save();
+    res.redirect('/');
 });
 
 
